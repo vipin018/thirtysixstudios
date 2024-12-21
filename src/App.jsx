ff0c9af4855b0800a4c430bb5635e9c9ace84016
vipin018 <vipinpathak018@gmail.com>
2024-12-21 17:54:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='w-full min-h-screen bg-black text-white'>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
