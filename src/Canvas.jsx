da0a271128cda3b8569f313ada88b3b47a30099a
vipin018 <vipinpathak018@gmail.com>
2024-12-21 21:22:07 +0530

    added new funcn

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,24 +1,31 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import canvasImg from './canvasImg';
-function Canvas() {
-    useEffect(() => {
-        const canvasRef = useRef(null);
 
+
+function Canvas() {
+    const [index, setIndex] = useState({ value: 0 });
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
+        const img = new Image();
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        const img = new Image();
-        img.src = canvasImg;
+        img.src = canvasImg[index.value];
         img.onload = () => {
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         }
+    }, [index]);
 
-    });
     return (
-        <canvas ref={canvasRef} id="canvas" >
-        </canvas>
+        <canvas ref={canvasRef}
+            id="canvas"
+            className='w-[18rem] h-[18rem]'
+        ></canvas>
     )
 }
+
+
 
 export default Canvas;
